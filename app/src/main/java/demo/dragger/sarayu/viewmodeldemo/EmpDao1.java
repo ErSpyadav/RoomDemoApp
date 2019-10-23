12c5dd189dca6f3c463659f9bf9c13198fe14a92
SarayuYadaw <sarary264@gmail.com>
2019-10-23 23:51:20 +0530

    test

---
+++ app/src/main/java/demo/dragger/sarayu/viewmodeldemo/EmpDao1.java
@@ -0,0 +1,25 @@
+package demo.dragger.sarayu.viewmodeldemo;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface EmpDao1 {
+    @Insert
+    void insertOnlySingleEmp(Employee emp);
+    @Insert
+    void insertOnlyMultipleEmp(List<Employee> emp);
+
+    @Query ("SELECT * FROM Employee WHERE empId = :empId")
+    Employee fetchOneEmpbyMovieId(int empId);
+
+    @Update
+    void updateMovie(Employee emp);
+    @Delete
+    void deleteMovie(Employee emp);
+}
